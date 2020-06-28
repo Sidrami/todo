d22bdadea37939b4900ed36c8d21fc70c4c6938f
sidrami <toku.ryo@ruri.waseda.jp>
2020-06-29 04:27:36 +0900

    Fix file paths

--- src/main.js
+++ src/main.js
@@ -1,8 +1,23 @@
-import Vue from 'vue'
-import App from './App.vue'
+// https://jp.vuejs.org/v2/examples/todomvc.html
+var STORAGE_KEY = "todos-vuejs-demo";
+var todoStorage = {
+  fetch: function() {
+    var todo = JSON.parse(localStorage.getItem(STORAGE_KEY) || "[]");
+    todo.forEach(function(todo, index) {
+      todo.id = index;
+    });
+    todoStorage.uid = todo.length;
+    return todo;
+  },
+  save: function(todo) {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(todo));
+  },
+};
 
-Vue.config.productionTip = false
-
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+const app = new Vue({
+  el: "#app",
+  data: {
+    todo: [],
+  },
+  methods: {},
+});
