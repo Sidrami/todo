d02c6e478ed1b8096c0ccfeb8c1e62912cb7df3f
sidrami <toku.ryo@ruri.waseda.jp>
2020-06-29 12:31:16 +0900

    add todo method

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
+/* eslint-disable no-unused-vars */
+import Vue from "vue";
+
 // https://jp.vuejs.org/v2/examples/todomvc.html
-var STORAGE_KEY = "todos-vuejs-demo";
+var STORAGE_KEY = "todo";
 var todoStorage = {
   fetch: function() {
     var todo = JSON.parse(localStorage.getItem(STORAGE_KEY) || "[]");
@@ -14,10 +17,38 @@ var todoStorage = {
   },
 };
 
-const app = new Vue({
+new Vue({
   el: "#app",
+
   data: {
     todo: [],
   },
-  methods: {},
+
+  methods: {
+    doAdd: function(event, value) {
+      var comment = this.$refs.comment;
+      if (!comment.value.length) {
+        return;
+      }
+      this.todo.push({
+        id: todoStorage.uid++,
+        comment: comment.value,
+        state: 0,
+      });
+      comment.value = "";
+    },
+
+    watch: {
+      todo: {
+        handler: function(todo) {
+          todoStorage.save(todo);
+        },
+        deep: true,
+      },
+    },
+
+    created() {
+      this.todo = todoStorage.fetch();
+    },
+  },
 });
